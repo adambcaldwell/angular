6050c622e763bd0a73d8dcb84ca10e2c87afffb3
Adam Caldwell <adambcaldwell25@gmail.com>
2016-06-16 17:34:06 -0400

    added working search, included font awesome icons, proper encoding for searches with spaces

--- webapp/js/http-requests.js
+++ webapp/js/http-requests.js
@@ -9,20 +9,27 @@ var timeout = 15000;
 httpRequests.controller('requests', ['$scope', '$filter', '$timeout', '$http', '$log',
     function ($scope, $filter, $timeout, $http, $log) {
 
-    $scope.search = 'Fears';
+        $scope.searchText = '';
+        $scope.loading = false;
 
-    // Spotify API URL: https://api.spotify.com
-    var url = 'https://api.spotify.com/';
+        // Spotify API URL: https://api.spotify.com
+        var url = 'https://api.spotify.com/';
 
-    // Search - v1/search
-    $timeout(function() {
-        $http.get( url + 'v1/search?q="' + $scope.search + '"&type=track')
-            .success(function (result) {
-                $scope.searchResult = result.tracks.items;
-                $log.info($scope.searchResult);
-            })
-            .error(function (data, status) {
-                $log.error(data);
-            });
-    }, timeout);
-}]);
+        // Search - v1/search
+        $scope.search = function () {
+            $scope.loading = true;
+            $log.info("Loading: " + $scope.loading);
+            $timeout(function () {
+                $http.get(url + 'v1/search?q="' + encodeURIComponent($scope.searchText) + '"&type=track')
+                    .success(function (result) {
+                        $scope.searchResult = result.tracks.items;
+                        $log.info($scope.searchResult);
+                        $scope.loading = false;
+                    })
+                    .error(function (data, status) {
+                        $log.error(data);
+                        $scope.loading = false;
+                    });
+            }, timeout);
+        }
+    }]);
