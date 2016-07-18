0738897f81b69d23dbd4272b326d27ef770e934f
adam.caldwell <adambcaldwell25@gmail.com>
2016-07-18 17:02:41 -0400

    transclusion

--- webapp/js/compile.js
+++ webapp/js/compile.js
@@ -36,7 +36,7 @@ compile.service('nameService', function () {
 // IMPORTANT: This is how you should always define the controller as it prevents minification from breaking it
 compile.controller('mainController', ['$scope', '$log', 'nameService', function ($scope, $log, nameService) {
 
-    $scope.person = [
+    $scope.people = [
         {
             name: 'John Doe',
             address: '555 Main St.',
@@ -75,56 +75,54 @@ compile.controller('secondController', ['$scope', '$log', 'nameService', '$route
 ]);
 
 // Model for directive
-compile.directive("searchResult", function(){
+compile.directive("searchResult", function() {
     return {
-        restrict: 'AECM', // Attribute(A), Element(E), Class(C), Comment(M)
-        templateUrl: 'searchresults.html',
-        replace: true,  // replace the element in the dom that indicates where this will go
+        restrict: 'AECM',
+        templateUrl: 'searchresult.html',
+        replace: true,
         scope: {
-            // Isolating the Scope - this is now the model for the scope - prevents from accidentally touching the scope of a page
-            personName: '@', // Text (@)
-            personAddress: '@'
+            personObject: "=",
+            formattedAddressFunction: "&"
         },
+        transclude: true,
         // Compile
-        compile: function(elem, attrs) {
-            console.log('Compiling...');
-            // You can use this to modify elements prior to them being displayed
-            // elem.removeAttr('class');
-            console.log(elem.html);
-
-            return {
-                // Avoid modifying this (per Angular JS documentation)
-                // pre: function(scope, elements, attrs) {
-                //     console.log('Pre-linking...');
-                //     console.log(elements);
-                // },
-
-                // This is safer to modify
-                post: function(scope, element, attrs){
-                    // If the directive requires actual code to handle a situation you'd do it here.
-                    console.log('Post-linking...');
-
-                    console.log(scope);
-                    if (scope.personObject.name == 'Jane Doe') {
-                        elements.removeAttr('class');
-                    }
-
-                    console.log(elements);
-                }
-            }
-        },
+        // compile: function(elem, attrs) {
+        //     console.log('Compiling...');
+        //     // You can use this to modify elements prior to them being displayed
+        //     // elem.removeAttr('class');
+        //     console.log(elem.html);
+        //
+        //     return {
+        //         // Avoid modifying this (per Angular JS documentation)
+        //         // pre: function(scope, elements, attrs) {
+        //         //     console.log('Pre-linking...');
+        //         //     console.log(elements);
+        //         // },
+        //
+        //         // This is safer to modify
+        //         post: function(scope, element, attrs){
+        //             // If the directive requires actual code to handle a situation you'd do it here.
+        //             console.log('Post-linking...');
+        //
+        //             console.log(scope);
+        //             if (scope.personObject.name == 'Jane Doe') {
+        //                 elements.removeAttr('class');
+        //             }
+        //
+        //             console.log(elements);
+        //         }
+        //     }
+        // },
 
         // NOTE: Be cautious about speed of code in this, as it will look at every element in the list
-        // An Empty Compile returning the post-link - a shortcut to the post-link (which is safest to modify)
-        link: function(scope, element, attrs){
+        // An Empty Compile returning the post-link - a shortcut to the compile:post-link (which is safest to modify)
+        link: function(scope, elements, attrs){
             // If the directive requires actual code to handle a situation you'd do it here.
             console.log('Linking...');
-
             console.log(scope);
             if (scope.personObject.name == 'Jane Doe') {
-                elements.removeAttr('class');
+                elements.addClass('alert alert-info');
             }
-
             console.log(elements);
         }
     }
