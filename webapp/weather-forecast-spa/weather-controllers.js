b0ec3eb8cb4e40456c1c4a08a06a75538dbbc637
adam.caldwell <adambcaldwell25@gmail.com>
2016-07-19 13:46:15 -0400

    weather forecast app - using a form

--- webapp/weather-forecast-spa/weather-controllers.js
+++ webapp/weather-forecast-spa/weather-controllers.js
@@ -3,15 +3,20 @@
  */
 
 // - - - - - CONTROLLERS - - - - -
-weatherApp.controller('home', ['$scope', '$log', 'cityService', function ($scope, $log, cityService) {
+weatherApp.controller('home', ['$scope', '$log', '$location', 'cityService',
+    function ($scope, $log, $location, cityService) {
 
-    $scope.city = cityService.city;
+        $scope.city = cityService.city;
 
-    $scope.$watch('city', function(){
-        cityService.city = $scope.city;
-    });
+        $scope.$watch('city', function () {
+            cityService.city = $scope.city;
+        });
 
-}]);
+        $scope.submit = function () {
+            $location.path("/forecast");
+        }
+
+    }]);
 
 weatherApp.controller('forecast', ['$scope', '$log', '$resource', '$routeParams', 'cityService',
     function ($scope, $log, $resource, $routeParams, cityService) {
@@ -21,17 +26,21 @@ weatherApp.controller('forecast', ['$scope', '$log', '$resource', '$routeParams'
 
         $scope.weatherAPI =
             $resource('http://api.openweathermap.org/data/2.5/forecast/daily',
-                { callback: "JSON_CALLBACK" },
-                { get: { method: "JSONP" } }
+                {callback: "JSON_CALLBACK"},
+                {get: {method: "JSONP"}}
             );
 
-        $scope.weatherResults = $scope.weatherAPI.get({ q: $scope.city, cnt: $scope.days, APPID: 'e7b65f864cab83dd69d97726cd84adb0' });
+        $scope.weatherResults = $scope.weatherAPI.get({
+            q: $scope.city,
+            cnt: $scope.days,
+            APPID: 'e7b65f864cab83dd69d97726cd84adb0'
+        });
 
-        $scope.convertToFahrenheit = function(degK) {
+        $scope.convertToFahrenheit = function (degK) {
             return Math.round((1.8 * (degK - 273)) + 32);
         };
 
-        $scope.convertToDate = function(dt) {
+        $scope.convertToDate = function (dt) {
             return new Date(dt * 1000);
         };
     }]);
