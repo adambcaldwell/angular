114b640f459890ece2272a1f731664a2415799f2
adam.caldwell <adambcaldwell25@gmail.com>
2016-07-19 12:38:22 -0400

    weather forecast app - adding directive

--- webapp/weather-forecast-spa/weather.js
+++ webapp/weather-forecast-spa/weather.js
@@ -78,3 +78,18 @@ weatherApp.service('cityService', function(){
         return self.city;
     }
 });
+
+// - - - - - DIRECTIVES - - - - -
+weatherApp.directive("weatherResult", function(){
+    return {
+        restrict: 'E',
+        templateUrl: 'weatherresults.html',
+        replace: true,
+        scope: {
+            weatherDay: '=',
+            convertToStandard: '&',
+            convertToDate: '&',
+            dateFormat: '@'
+        }
+    }
+});
