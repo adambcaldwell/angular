f12919f951f0c2ee6ae181120c540cec52518df4
adam.caldwell <adambcaldwell25@gmail.com>
2016-07-19 13:52:53 -0400

    weather forecast app - moving api call to service

--- webapp/weather-forecast-spa/weather-controllers.js
+++ webapp/weather-forecast-spa/weather-controllers.js
@@ -18,23 +18,13 @@ weatherApp.controller('home', ['$scope', '$log', '$location', 'cityService',
 
     }]);
 
-weatherApp.controller('forecast', ['$scope', '$log', '$resource', '$routeParams', 'cityService',
-    function ($scope, $log, $resource, $routeParams, cityService) {
+weatherApp.controller('forecast', ['$scope', '$log', '$routeParams', 'cityService', 'weatherService',
+    function ($scope, $log, $routeParams, cityService, weatherService) {
 
         $scope.city = cityService.city;
         $scope.days = $routeParams.days || 2;
 
-        $scope.weatherAPI =
-            $resource('http://api.openweathermap.org/data/2.5/forecast/daily',
-                {callback: "JSON_CALLBACK"},
-                {get: {method: "JSONP"}}
-            );
-
-        $scope.weatherResults = $scope.weatherAPI.get({
-            q: $scope.city,
-            cnt: $scope.days,
-            APPID: 'e7b65f864cab83dd69d97726cd84adb0'
-        });
+        $scope.weatherResults = weatherService.getWeather($scope.city, $scope.days);
 
         $scope.convertToFahrenheit = function (degK) {
             return Math.round((1.8 * (degK - 273)) + 32);
--- webapp/weather-forecast-spa/weather-directives-services.js
+++ webapp/weather-forecast-spa/weather-directives-services.js
@@ -2,17 +2,29 @@
  * @author adam.caldwell
  */
 // - - - - - SERVICES - - - - -
-weatherApp.service('cityService', function(){
+weatherApp.service('cityService', function () {
     var self = this;
     self.city = 'Atlanta, GA';
 
-    this.getCity = function() {
+    this.getCity = function () {
         return self.city;
     }
 });
 
+weatherApp.service('weatherService', ['$resource', function ($resource) {
+    this.getWeather = function (city, days) {
+
+        var weatherAPI =
+            $resource('http://api.openweathermap.org/data/2.5/forecast/daily',
+                {callback: "JSON_CALLBACK"},
+                {get: {method: "JSONP"}}
+            );
+        return weatherAPI.get({q: city, cnt: days, APPID: 'e7b65f864cab83dd69d97726cd84adb0'});
+    };
+}]);
+
 // - - - - - DIRECTIVES - - - - -
-weatherApp.directive("weatherResult", function(){
+weatherApp.directive("weatherResult", function () {
     return {
         restrict: 'E',
         templateUrl: 'weatherresults.html',
